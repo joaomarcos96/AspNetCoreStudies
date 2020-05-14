0771132627ebf4cfade198d2c4bd2020c26a0f3c
Joao Marcos <jao.markos@hotmail.com>
2020-05-14 19:55:59 -0300

    Fix TestFixture to use SQLite in-memory

--- AspNetCoreStudies.Api/Startup.cs
+++ AspNetCoreStudies.Api/Startup.cs
@@ -23,18 +23,16 @@ namespace AspNetCoreStudies.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
 
             services.AddDbContext<ApplicationDbContext>(options =>
             {
-                options.UseSqlite(Configuration.GetConnectionString("Data Source=app.db"));
+                options.UseSqlite("DataSource=app.db");
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
--- AspNetCoreStudies.Tests/TestFixture.cs
+++ AspNetCoreStudies.Tests/TestFixture.cs
@@ -1,39 +1,36 @@
 using System;
-using System.IO;
+using System.Data.Common;
 using System.Net.Http;
 using AspNetCoreStudies.Api;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNetCoreStudies.Tests
 {
     public class TestFixture : IDisposable
     {
+        private DbConnection _connection;
+
         private readonly TestServer _server;
 
         public HttpClient Client { get; }
-
-        private readonly string _dbName = $"{Guid.NewGuid()}.db";
 
         public TestFixture()
         {
             var builder = new WebHostBuilder()
                 .UseStartup<Startup>()
-                .ConfigureAppConfiguration((context, config) =>
-                {
-                    config
-                        .SetBasePath(Directory.GetCurrentDirectory())
-                        .AddJsonFile("appsettings.json", true, true)
-                        .AddEnvironmentVariables();
-                })
+                .UseEnvironment("Testing")
                 .ConfigureServices(services =>
                 {
+                    _connection = new SqliteConnection("DataSource=file::memory:?cache=shared");
+                    _connection.Open();
+
                     services.AddDbContext<ApplicationDbContext>(options =>
                     {
-                        options.UseSqlite($"Data Source={_dbName}");
+                        options.UseSqlite(_connection);
                     });
 
                     var serviceProvider = services.BuildServiceProvider();
@@ -43,9 +40,9 @@ namespace AspNetCoreStudies.Tests
                         var scopedServices = scope.ServiceProvider;
                         var dbContext = scopedServices.GetRequiredService<ApplicationDbContext>();
 
+                        dbContext.Database.EnsureDeleted();
                         dbContext.Database.EnsureCreated();
                     }
-
                 });
 
             _server = new TestServer(builder);
@@ -55,9 +52,14 @@ namespace AspNetCoreStudies.Tests
 
         public void Dispose()
         {
-            File.Delete(_dbName);
             Client.Dispose();
             _server.Dispose();
+
+            if (_connection != null)
+            {
+                _connection.Close();
+                _connection = null;
+            }
         }
     }
 }
